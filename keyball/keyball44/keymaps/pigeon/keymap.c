05fdafb0566ace3af6674926ccd67cb7a970aaf3
Mint <skylove.exe@gmail.com>
2023-04-04 12:51:46 +0900

    LEDを無効化し、WINDOWS用キーをベースにした

--- keyball/keyball44/keymaps/pigeon/keymap.c
+++ keyball/keyball44/keymaps/pigeon/keymap.c
@@ -30,6 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // 長いキーをマクロに
 #define SCRLTRG LT(_SCROLL,KC_BTN3)
 #define NOSPACE _______
+#define DELETED _______
 #define NEXTTAB LCTL(KC_TAB)
 #define PREVTAB LCTL(LSFT(KC_TAB))
 #define NEXTXLS LCTL(KC_PGUP)
@@ -56,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT ,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_QUOT,
-                      _______, KC_LALT, KC_LGUI, IMEOFF , KC_SPC , KC_RGUI, IMEON  , NOSPACE, NOSPACE, _______
+                      DELETED, DELETED, KC_LALT, IMEOFF , KC_SPC , KC_RGUI, IMEON  , NOSPACE, NOSPACE, DELETED
   ),
 
   [1] = LAYOUT_universal(
@@ -74,7 +75,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
   [3] = LAYOUT_universal(
-    _______, _______, _______, _______, _______, _______,                   PREVXLS, _______, _______, NEXTXLS, _______, _______, 
+    _______, _______, _______, _______, _______, _______,                   _______, PREVXLS, NEXTXLS, _______, _______, _______, 
     _______, _______, _______, _______, _______, _______,                   PREVTAB, KC_BTN1, KC_BTN2, NEXTTAB, _______, _______, 
     _______, _______, _______, _______, _______, _______,                   KC_BTN4, KC_BTN3, SCRLTRG, KC_BTN5, _______, _______, 
                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
