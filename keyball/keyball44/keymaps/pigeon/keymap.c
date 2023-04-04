9851b1921df1ec7be0b4907657f27a421e8da223
Mint <skylove.exe@gmail.com>
2023-04-04 12:56:51 +0900

    WINモード有効化
    GUIとALTを入れ替えられれば不要かも

--- keyball/keyball44/keymaps/pigeon/keymap.c
+++ keyball/keyball44/keymaps/pigeon/keymap.c
@@ -21,11 +21,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "quantum.h"
 
 // レイヤー定義
-#define _BASE 0
-#define _LOWER 1
-#define _RAISE 2
-#define _MOUSE 3
-#define _SCROLL 4
+#define _MAC 0
+#define _WINDOWS 1
+#define _LOWER 2
+#define _RAISE 3
+#define _MOUSE 4
+#define _SCROLL 5
 
 // 長いキーをマクロに
 #define SCRLTRG LT(_SCROLL,KC_BTN3)
@@ -53,35 +54,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //                   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
 
 
-  [0] = LAYOUT_universal(
+  [_MAC] = LAYOUT_universal(
     KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
     KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT ,
     KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_QUOT,
                       DELETED, DELETED, KC_LALT, IMEOFF , KC_SPC , KC_RGUI, IMEON  , NOSPACE, NOSPACE, DELETED
   ),
 
-  [1] = LAYOUT_universal(
+  [_WINDOWS] = LAYOUT_universal(
+    KC_TAB , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_BSPC,
+    KC_LCTL, KC_A   , KC_S   , KC_D   , KC_F   , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_ENT ,
+    KC_LSFT, KC_Z   , KC_X   , KC_C   , KC_V   , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_QUOT,
+                      DELETED, DELETED, KC_LGUI, IMEOFF , KC_SPC , KC_RALT, IMEON  , NOSPACE, NOSPACE, DELETED
+  ),
+
+  [_LOWER] = LAYOUT_universal(
     KC_ESC , _______, KC_F2  , KC_F3  , KC_F4  , KC_F5  ,                   KC_1   , KC_2   , KC_3   , KC_4   , KC_5   , KC_DEL , 
     _______, KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 ,                   KC_6   , KC_7   , KC_8   , KC_9   , KC_0   , _______, 
     _______, KC_F11 , KC_F12 , _______, _______, _______,                   KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV ,
                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [2] = LAYOUT_universal(
+  [_RAISE] = LAYOUT_universal(
     KC_ESC , _______, _______, _______, _______, _______,                   KC_HOME, KC_PGDN, KC_PGUP,  KC_END, _______, KC_DEL , 
     _______,CPI_D100,SCRL_DVI,SCRL_DVD,CPI_I100, _______,                   KC_LEFT, KC_DOWN, KC_UP  ,KC_RIGHT, _______, _______, 
     _______, _______, _______, _______, _______, _______,                   KC_MINS, KC_EQL , KC_LBRC, KC_RBRC, KC_BSLS, KC_GRV ,
                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [3] = LAYOUT_universal(
+  [_MOUSE] = LAYOUT_universal(
     _______, _______, _______, _______, _______, _______,                   _______, PREVXLS, NEXTXLS, _______, _______, _______, 
     _______, _______, _______, _______, _______, _______,                   PREVTAB, KC_BTN1, KC_BTN2, NEXTTAB, _______, _______, 
     _______, _______, _______, _______, _______, _______,                   KC_BTN4, KC_BTN3, SCRLTRG, KC_BTN5, _______, _______, 
                       _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
   ),
 
-  [4] = LAYOUT_universal(
+  [_SCROLL] = LAYOUT_universal(
         MAC, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
     WINDOWS, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
    KBC_SAVE, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______, 
@@ -158,7 +166,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case MAC:
       if (record->event.pressed) {
         isWindows = false;
-        //persistent_default_layer_set(1UL<<_MAC);
+        persistent_default_layer_set(1UL<<_MAC);
         SEND_STRING("Mac Mode");
       }
       return false;
@@ -166,7 +174,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case WINDOWS:
       if (record->event.pressed) {
         isWindows = true;
-        //persistent_default_layer_set(1UL<<_WINDOWS);
+        persistent_default_layer_set(1UL<<_WINDOWS);
         SEND_STRING("Windows Mode");
       }
       return false;
